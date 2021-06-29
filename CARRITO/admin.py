5fb0421ac43c6fed2849683deb258567bc120d92
Sebastian <sebastiangalvan.ar@gmail.com>
2021-06-28 23:08:48 -0300

    [REFACTOR] Registrar los modelos den admin.py

--- CARRITO/admin.py
+++ CARRITO/admin.py
@@ -1,3 +1,7 @@
+from CARRITO.models import Carrito, Categoria, Producto
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Carrito)
