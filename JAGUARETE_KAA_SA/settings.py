2b15c3734e9bfe164f808475760aefc1f9e37478
Sebastian <sebastiangalvan.ar@gmail.com>
2021-07-01 16:49:02 -0300

    [REFACTOR] Modificar archivos del proyecto e incluir los archivos de las apps en urls.py y settings.py

--- JAGUARETE_KAA_SA/settings.py
+++ JAGUARETE_KAA_SA/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-# from decouple import config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CARRITO'
+    'CARRITO',
+    'USUARIOS'
   
 ]
 
@@ -131,3 +132,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST= "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
--- JAGUARETE_KAA_SA/urls.py
+++ JAGUARETE_KAA_SA/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from JAGUARETE_KAA_SA.views import inicio
+
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
-
+    path('',include('CARRITO.urls')),
+    path('USUARIO',include('USUARIOS.urls'))
 ]
 
 
