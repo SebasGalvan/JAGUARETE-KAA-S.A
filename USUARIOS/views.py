655319d4ca03df9c35906fd1d8ada73404bdb60c
Sebastian <sebastiangalvan.ar@gmail.com>
2021-07-06 23:01:36 -0300

    [REFACTOR] Archivos template de USUARIO y views

--- USUARIOS/views.py
+++ USUARIOS/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
 from .forms import formularioLogin, formularioRegistro
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 
 def registro(request):
-    form = formularioRegistro
-    return render(request,'registration/registro.html',{'form':form})
+    
+    if request.POST:
+        
+        
+        user =  request.POST['username']
+        password =  request.POST['password']
+        nombre =  request.POST['nombre']
+        apellido =  request.POST['apellido']
+        email =  request.POST['email']
+        
+        usuario  = User.objects.create_user(user, email, password)
+        usuario.first_name = nombre
+        usuario.last_name = apellido
+
+        usuario.save()
+        
+        form = formularioLogin
+        return render(request,'registration/login.html',{'form':form})
+        
+    else:
+        form = formularioRegistro
+        return render(request,'registration/registro.html',{'form':form})
+
+
+
 
 def login(request):
     form = formularioLogin
