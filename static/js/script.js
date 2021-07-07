a25784c90a2431b6abb2d70d42a578b8270dbc57
Sebastian <sebastiangalvan.ar@gmail.com>
2021-07-06 23:05:01 -0300

    [ADD] Agregar archivo js

---
+++ static/js/script.js
@@ -0,0 +1,23 @@
+function mostrarSeleccon(){
+
+    val = document.querySelector('#id__categoria').value
+    val_num = Number(val)
+    console.log(val_num);
+    buscapPorCategoria(val_num)
+
+};
+
+const urlBase =  window.location.href
+console.log(urlBase);
+
+function buscapPorCategoria(id){
+    url = getURL(window.location.href)+ id
+    location.replace(url)
+}
+
+function getURL(url) { 
+    let arr = url.split("/");
+    ruta = `${arr[0]}//${arr[2]}/categoria/`
+    return ruta
+    }     
+            
