d577be07bc5bebed172a22b33420c2208388b480
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2023-05-20 16:28:35 -0300

    Fix libs

--- TIENDA/asgi.py
+++ TIENDA/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JAGUARETE_KAA_SA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TIENDA.settings')
 
 application = get_asgi_application()
--- TIENDA/wsgi.py
+++ TIENDA/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for JAGUARETE_KAA_SA project.
+WSGI config for TIENDA project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JAGUARETE_KAA_SA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TIENDA.settings')
 
 application = get_wsgi_application()
